25e033418455d29ce8dccfc2a1052e842a999fef
pawan <pawangrover904@gmail.com>
2026-05-28 13:36:15 +0530

    add cnd link font awesome and work on topbar.jsx

--- src/Component/Layout/Navbar/Topbar.jsx
+++ src/Component/Layout/Navbar/Topbar.jsx
@@ -1,7 +1,13 @@
-
-
 export default function Topbar() {
   return (
-    <div>Topbar</div>
-  )
+    <>
+      <div className="topbar-container">
+        <h6>
+          <i className="fa-solid fa-phone-volume"></i> Emergency Call:1236547890
+        </h6>
+        <h6>10:00am to 10:10px</h6>
+        <h6>help@support.com </h6>
+      </div>
+    </>
+  );
 }
