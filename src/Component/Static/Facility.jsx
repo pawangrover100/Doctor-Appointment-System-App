0f504c329110b5e876aa0891988de7fa9cd1c7a2
pawan <pawangrover904@gmail.com>
2026-05-29 14:38:01 +0530

    add Facility.css and facility.json

---
+++ src/Component/Static/Facility.jsx
@@ -0,0 +1,15 @@
+import "./facility.css";
+function Facility() {
+  return (
+    <>
+      <h1>Facilities</h1>
+      <div className="facility-container">
+        <div className="card">
+          
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Facility;
